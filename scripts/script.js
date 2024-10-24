e9503fa3e84d46f43c69714ca84fe17e75f77e11
Kendra Dias <kendra.dias@hotmail.com>
2024-10-24 12:26:53 -0700

    added templates for required methods in game object

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,32 @@
 
 // Game Object 
 const game = {
-    title: "Kendra's Game"
+    title: "Kendra's Game",
+    isRunning: false,
+    //properties
+    gameBoard: document.getElementById('game-board'),
+    scoreBoard: document.getElementById('score-board'),
+    playerForm: document.getElementById('player-name-input'),
+    joinGame: document.getElementById('join-game'),
+    startGame: document.getElementById('start-game'),
+    scorePointsButton: document.getElementById('score-points'),
+    startGameButton: document.getElementById('start-game'),
+    playerNameDisplay: document.getElementById('player-name'),
+    playerScoreDisplay: document.getElementById('player-score'),
+    //methods
+    toggleRunning: function () {
+
+    },
+    
+    updatePlayerName: function () {
+
+    },
+
+    updatePlayerScore: function () {
+
+    },
+
+
 }
 
 // Player Object
@@ -10,3 +35,6 @@ const player = {
     name: "",
     score: 0
 }
+
+// Event Listeners
+
